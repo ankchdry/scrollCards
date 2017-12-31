5e8dd6d600352a3803943e651cf812ed9dd3873b
Ankit Chaudhary <ank.chdry@gmail.com>
2017-12-31 14:41:03 +0530

    Initial Commit

---
+++ scrollCards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  scrollCards
+//
+//  Created by Ankit  Chaudhary on 31/12/17.
+//  Copyright © 2017 Ankit  Chaudhary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
